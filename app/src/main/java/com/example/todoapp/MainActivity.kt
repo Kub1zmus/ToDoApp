dc548c9b0b18515d7cdf3ad122333ef7b5f0e947
Jakub Adam <jakubadam242@gmail.com>
2026-04-13 22:31:01 +0200

    ToDoApp v0.0.1 - version 0, patch 1
    ----------------------------------------------------------------------------------------
    
    Patch notes:
    * Resolved bug, where more tasks with the same name could be created, which lead to another bug, where for example, if a task number 3 was clicked as done, task number 1 with the same name would get marked as done
    * Added removing the task 1 second after being marked as done (task marked as done does still become crossed out)
    
    ----------------------------------------------------------------------------------------
    
    CODERS-ONLY:
    * Added in-code notes for better understanding of the code in the future

--- app/src/main/java/com/example/todoapp/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -29,6 +29,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -44,15 +45,23 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.todoapp.ui.theme.ToDoAppTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
+
+// Vytvorí data class Task, do ktorej sa budú zapisovať všetky úlohy
 data class Task(
     var task: String,
+    // TODO -> Pridaj, aby sa na úlohe mohol pridať aj custom deadline
     var isDone: Boolean = false
 )
 
+// Vytvorí list úloh, do ktorého sa budú úlohy zapisovať a z ktorého sa budú vyberať dáta úloh pre UI
+// List môže obsahovať len dátový typ Task
 var taskList = mutableStateListOf<Task>()
 
-@OptIn(ExperimentalMaterial3Api::class)
+
+// Hlavná funkcia, v ktorej sa tvorí Compose
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -71,11 +80,12 @@ fun ToDoScreen(
     modifier: Modifier = Modifier
     )
 {
-    var textInput by remember { mutableStateOf("") }
+    val scope = rememberCoroutineScope()
 
+    // Vytvorí template pre hlavné rozhranie aplikácie
     Scaffold(
         topBar = {
-            // Tu definuj lištu
+            // TopBar aplikácie s textom v strede
             CenterAlignedTopAppBar(
                 title = {
                     Text(
@@ -84,33 +94,43 @@ fun ToDoScreen(
                         fontWeight = FontWeight.Bold
                     )
                 },
+                // Program nastaví farby na hlavné farby pre theme apky/telefónu
                 colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
                     containerColor = MaterialTheme.colorScheme.primaryContainer
                 )
             )
         }
     ) { innerPadding ->
+        // Vytvorí Stĺpec pre okno na zadávanie úloh a pre list úloh, ktoré budú zobrazené
         Column(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(innerPadding)
         ) {
 
+            // Vykoná funkciu taskCreate, v ktorej je definované zobrazenie
             TaskCreate()
 
             LazyColumn(
                 modifier = Modifier.fillMaxSize()
             ) {
-                items(taskList) { task ->
+                items(taskList) { task ->    // items() -> zoberie dáta z daného... niečoho a urobí s nimi to, čo je nižšie
                     TaskRow(
-                        task,
-                        onCheckChanged = { newValue ->
-                            // Tu povieme zoznamu: "Nájdi túto úlohu a prepíš jej stav"
+                        task = task,
+                        onCheckChanged = { isDoneChange ->
+                            // Program nájde úlohu, ktorej stav sa ide zmeniť
                             val index = taskList.indexOf(task)
 
                             // Ak program nenájde úlohu s daným indexom, tak bude index -1 -> keď program nájde úlohu, pracuje s jej indexom
+                            // Ak sa checkbox zaklikne, zmení isDone vlastnosť danej úlohy, počká 1s a program vymaže danú hotovú úlohu
                             if (index != -1) {
-                                taskList[index] = task.copy(isDone = newValue)
+                                scope.launch {
+                                    taskList[index] = task.copy(isDone = isDoneChange) // Program nahradí danú úlohu jeho kópiou,
+                                                                                       // no s novou hodnotou vlastnosti isDone
+                                    delay(1000L)
+                                    taskList.remove(taskList[index])
+                                }
+
                             }
                         }
                     )
@@ -120,6 +140,8 @@ fun ToDoScreen(
     }
 }
 
+
+// Funkcia definuje, ako má vyzerať UI pre jednu úlohu
 @Composable
 fun TaskRow(
     task: Task,
@@ -128,6 +150,7 @@ fun TaskRow(
     Row(
         verticalAlignment = Alignment.CenterVertically,
     ) {
+        // Checkbox pre zakliknutie, či je úloha hotová alebo nie
         Checkbox(
             checked = task.isDone,
             onCheckedChange = { onCheckChanged(it) },
@@ -138,6 +161,7 @@ fun TaskRow(
                 .fillMaxWidth()
                 .padding(16.dp)
         ) {
+            // Text s názvom úlohy
             Text(
                 text = task.task,
                 fontWeight = FontWeight.Bold,
@@ -149,19 +173,20 @@ fun TaskRow(
                         TextStyle.Default
                     }
             )
+            // Text pre deadline úlohy  TODO -> Pridaj, aby sa na úlohe mohol pridať aj custom deadline
             Text(
                 text = stringResource(R.string.deadline),
                 color = Color.Red
             )
         }
     }
+    // Čiara, ktorá oddelí dané úlohy od seba
     HorizontalDivider(thickness = 0.5.dp, color = Color.LightGray)
 }
 
+// Funkcia definuje UI pre zadávanie novej úlohy
 @Composable
-fun TaskCreate(
-    modifier: Modifier = Modifier
-)
+fun TaskCreate()
 {
     var textInput by remember { mutableStateOf("") }
 
@@ -171,6 +196,7 @@ fun TaskCreate(
             .padding(20.dp),
         verticalAlignment = Alignment.CenterVertically,
     ) {
+        // Textové pole na zadanie úlohy
         TextField(
             value = textInput,
             label = { Text(stringResource(R.string.task_name_input)) },
@@ -185,21 +211,21 @@ fun TaskCreate(
                 .weight(1f),
             keyboardActions = KeyboardActions(
                 onDone = {
-                    if (textInput.isNotBlank())
-                    {
-                        taskList.add(Task(task = textInput))
-                        textInput = ""
-                    }
+                    // Vykoná funkciu, ktorá odosiela novú úlohu do listu úloh
+                    done(
+                        textInput = textInput,
+                        onSuccess = {textInput = ""})
                 }
             )
         )
+        // Tlačidlo done - odoslanie novej úlohy do listu
         FloatingActionButton(
             onClick = {
-                if (textInput.isNotBlank())
-                {
-                    taskList.add(Task(task = textInput))
-                    textInput = ""
-                }
+                // Vykoná funkciu, ktorá odosiela novú úlohu do listu úloh
+                done(
+                    textInput = textInput,
+                    onSuccess = {textInput = ""}
+                )
             },
         ) {
             Icon(
@@ -208,5 +234,23 @@ fun TaskCreate(
             )
         }
     }
+    // Čiara, ktorá oddelí UI pre zadanie úlohy od listu úloh
     HorizontalDivider(thickness = 0.5.dp, color = Color.LightGray)
+}
+
+
+// Keďže sa odoslanie novej úlohy do listu dá urobiť aj cez enter na klávesnici, aj cez tlačidlo done, vytvoril som novú funkciu pre jednoduchosť a efektivitu kódu
+fun done(
+    textInput: String,
+    onSuccess: () -> Unit
+)
+{
+    val trimmedInput = textInput.trim()
+    val isATask = taskList.any { it.task.equals(trimmedInput, ignoreCase = true)}
+
+    if (textInput.isNotBlank() && !isATask)
+    {
+        taskList.add(Task(task = textInput))
+        onSuccess()
+    }
 }
